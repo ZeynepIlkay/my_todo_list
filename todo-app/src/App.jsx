68955a9e752a49f887e8262b967c8fbc8f48044a
ZeynepIlkay <ilkayzeynepp@gmail.com>
2024-09-09 15:29:54 +0300

    add button and background added.

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -5,11 +5,25 @@ function App() {
   
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className='min-h-screen flex items-center justify-center p-4 bg-custom-background bg-center bg-cover'>
+      <div className='bg-gray-100 p-6 rounded shadow-md w-full max-w-lg lg:w-1/4'> 
+        <h1 className = 'text-3xl font-bold text-center mb-4'>Todo List </h1>
+        <div className='flex'>
+          <input type="text"
+          placeholder='Add a todo'
+          className='py-2 px-4 border rounded w-full focus:outline-none mr-2'
+
+          />
+          <button className='bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] rounded-full text-white py-2 py-4'>
+            
+            Add
+          </button>
+    
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
--- todo-app/tailwind.config.js
+++ todo-app/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (_) => ({
+        'custom-background': "url('./src/assets/pexels-eberhardgross-844297.jpg')"
+      })
+    },
   },
   plugins: [],
 }
