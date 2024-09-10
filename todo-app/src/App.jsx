5c8376f8ddc309185344aa43cc706f6f9e228718
ZeynepIlkay <ilkayzeynepp@gmail.com>
2024-09-10 10:12:00 +0300

    Edit and remove added. Bugs fixed

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -5,9 +5,15 @@ import { FaPlus, FaPencilAlt, FaTrash } from 'react-icons/fa';
 
 function App() {
   const [todos, setTodos] = useState([
-    {id: 1, todo: 'Learn React'}
+    //{id: 1, todo: 'Learn React'}
   ]);
   const [input, setInput] = useState('');
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const setEdit = (index) => {
+    setInput(todos[index].todo);
+    setEditIndex(index);
+  };
 
   const addTodo = async () => {
     try {
@@ -17,6 +23,33 @@ function App() {
       }
     }catch (error){
       console.error(error.message);
+    }
+  }
+
+  const updateTodo = async () => {
+    try{
+      if (input.trim() != '') {
+        const updatedTodos = [...todos];
+        updatedTodos[editIndex].todo = input;
+        setTodos(updatedTodos);
+        setEditIndex(-1);
+        setInput('')
+      }
+
+    } catch(error) {
+      console.error(error.message);
+
+    }
+  }
+
+  const removeTodo = async (id) => {
+    let filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+
+    // Eğer silinen todo şu an düzenlenmekte olan todo ise, editIndex ve input'u sıfırla
+    if (editIndex !== -1 && todos[editIndex]?.id === id) {
+      setEditIndex(-1);
+      setInput('');
     }
   }
 
@@ -32,28 +65,14 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           />
-          <button onClick= {addTodo} className='bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] rounded-full text-white py-2 py-4 hover: from-pink-100 hover:to-red-700'>
+          <button onClick= {editIndex === -1 ? addTodo : updateTodo} className='bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] rounded-full text-white py-2 py-4 hover: from-pink-100 hover:to-red-700'>
             
-            <FaPlus/>
+            {editIndex === -1 ? <FaPlus/> : < FaPencilAlt/>}
           </button>
     
         </div>
 
-        <div className='bg-gray-100 p-6 rounded shadow-m w-full max-w-lg lg:w-1/4'>
-          
-          <ul>
-            {todos.map((todo, index) => (
-              <li key = {index} className='flex items-center justify-between bg-white p-3  rounded shadow-md mb-3'>
-              <span className='text-lg'>todo.todo</span>
-              <div>
-              <button className="mr-2 p-2 bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] text-white rounded-full hover: from-pink-100 hover:to-red-700"><FaPencilAlt/></button>
-              <button className="mr-2 p-2 bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] text-white rounded-full hover: from-pink-100 hover:to-red-700"><FaTrash/></button>
-              </div>
-            </li>
-            ))}
-            
-          </ul>
-        </div>
+        
 
       </div>
       
@@ -64,10 +83,14 @@ function App() {
           <ul>
             {todos.map((todo, index) => (
               <li key = {index} className='flex items-center justify-between bg-white p-3  rounded shadow-md mb-3'>
-              <span className='text-lg'>todo.todo</span>
+              <span className='text-lg'>{todo.todo}</span>
               <div>
-              <button className="mr-2 p-2 bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] text-white rounded-full hover: from-pink-100 hover:to-red-700"><FaPencilAlt/></button>
-              <button className="mr-2 p-2 bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] text-white rounded-full hover: from-pink-100 hover:to-red-700"><FaTrash/></button>
+              <button onClick={() => setEdit(index)} className="mr-2 p-2 bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] text-white rounded-full hover: from-pink-100 hover:to-red-700">
+                <FaPencilAlt/>
+              </button>
+              <button onClick={() => removeTodo(todo.id)} className="mr-2 p-2 bg-gradient-to-r from-[rgba(244,114,182,0.1)] to-[rgba(219,39,119,0.1)] text-white rounded-full hover: from-pink-100 hover:to-red-700">
+                <FaTrash/>
+              </button>
               </div>
             </li>
             ))}
